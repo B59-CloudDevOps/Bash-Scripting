d7b0fa31448fe466c54329b67122fb0527a7adef
CodingManoj <CodingManoj@gmail.com>
2025-03-06 19:58:18 -0500

    Bash Scripting ~ AutoPush

--- 02-print.sh
+++ 02-print.sh
@@ -4,3 +4,9 @@ echo hai
 echo hello
 echo welcome
 echo DevSecOps
+
+# Special Characters to do something
+#  /n : Move the cursor to the next line
+#  /t : Move the cursor to the next tab
+
+echo "hai /n hello /n welcome /n  DevSecOps"
