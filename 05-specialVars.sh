06193cf523efdafc034b9c89a82d31a3a41de854
CodingManoj <mvp2612@hotmail.com>
2025-03-07 20:04:17 -0500

    Bash scripting

--- 05-specialVars.sh
+++ 05-specialVars.sh
@@ -12,7 +12,8 @@ echo "Value of a is $a"
 echo "scriptName is $0"
 echo "Name of the team is $TEAM"
 echo "Name of the project is $PROJECT"
-echo "Number Of Arguments in script are $#"     # Prints the no of arguments used in the script 
+echo "Number Of Arguments in script are $#"                             # Prints the no of arguments used in the script 
+echo "Number Of Arguments in script are $@"                             # Prints the no of arguments used in the script 
 
 # $0 : Prints the name of the script you're running
 
