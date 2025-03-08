f062701ff14d00f2499b0a903003055b5dfcb79c
CodingManoj <mvp2612@hotmail.com>
2025-03-07 19:58:02 -0500

    Bash scripting

--- 05-specialVariables.sh
+++ 05-specialVariables.sh
@@ -20,8 +20,8 @@ echo "scriptName is $0"
 #        1     2     3    4    5     6    7      8    9      10
 # ( You supply up to 9 arguments)
 
-echo $0     # prints the scriptName
-echo "Number Of Arguments in script are $#"     # Prints the no of arguments used in the script 
-echo "Exit Status Of The Previous Command $?"                                         # Prints the exit code of the previous command 
-echo "Arguments In The Script Are $*"           # Prints all the arguments used in the script
-echo "Arguments In The Script Are $@"           # Prints all the arguments used in the script
+# echo $0     # prints the scriptName
+# echo "Number Of Arguments in script are $#"     # Prints the no of arguments used in the script 
+# echo "Exit Status Of The Previous Command $?"                                         # Prints the exit code of the previous command 
+# echo "Arguments In The Script Are $*"           # Prints all the arguments used in the script
+# echo "Arguments In The Script Are $@"           # Prints all the arguments used in the script
