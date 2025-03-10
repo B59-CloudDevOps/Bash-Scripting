c04fa2c443543a35a537a15743cfb87ca05d06cd
CodingManoj <CodingManoj@gmail.com>
2025-03-09 21:32:32 -0400

    Bash Scripting ~ AutoPush

---
+++ 13-if.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+echo -e "Demo on if conditions"
+
+ACTION=$1 
+
+# Demo on if condition
+# if [ "$ACTION" == "start" ]; then         # The below commands will only be exectuted if the condition is true.
+#     echo "Starting the service"
+# fi 
+
+# echo -e "Script Completed"
+
+# Demo on if else condition
+if [ "$ACTION" == "start" ]; then         # The below commands will only be exectuted if the condition is true.
+    echo "Starting the service"
+else
+    echo "Valid Arguments are start"
+    echo "Ex: bash $0 start"
+fi 
+
+echo -e "Script Completed"
