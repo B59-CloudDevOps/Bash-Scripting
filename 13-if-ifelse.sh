876935ed9b7aa09d51beebcba2a7b7ee918fa04e
CodingManoj <CodingManoj@gmail.com>
2025-03-09 21:42:30 -0400

    Bash Scripting ~ AutoPush

--- 13-if-ifelse.sh
+++ 13-if-ifelse.sh
@@ -26,10 +26,10 @@ if [ "$ACTION" == "start" ]; then         # The below commands will only be exec
     echo -e "\e[32m Starting the service \e[0m"
 
 elif [ "$ACTION" == "stop" ]; then
-    echo -e "\[31m Stopping the service \e[0m"
+    echo -e "\e[31m Stopping the service \e[0m"
 
 elif [ "$ACTION" == "restart" ]; then
-    echo -e "\[35mRestarting the service \e[0m"
+    echo -e "\e[35m Restarting the service \e[0m"
 else
     echo "Valid Arguments are start or stop or restart ONLY"
     echo -e "Ex: \n\t \e[33m bash $0 start  \e[0m"
