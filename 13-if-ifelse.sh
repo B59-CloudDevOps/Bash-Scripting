145ec848e5a8de611e81f31494530994f2cd4811
CodingManoj <CodingManoj@gmail.com>
2025-03-09 21:41:53 -0400

    Bash Scripting ~ AutoPush

--- 13-if-ifelse.sh
+++ 13-if-ifelse.sh
@@ -18,22 +18,21 @@ ACTION=$1
 #     echo "Valid Arguments are start"
 #     echo -e "Ex: \n\t \e[31m bash $0 start \e[0m"
 # fi 
-
 # echo -e "Script Completed"
 
 # Demo on else if  condition
+
 if [ "$ACTION" == "start" ]; then         # The below commands will only be exectuted if the condition is true.
-    echo "Starting the service"
+    echo -e "\e[32m Starting the service \e[0m"
 
 elif [ "$ACTION" == "stop" ]; then
-    echo "Stopping the service"
+    echo -e "\[31m Stopping the service \e[0m"
 
 elif [ "$ACTION" == "restart" ]; then
-    echo "Restarting the service"
-
+    echo -e "\[35mRestarting the service \e[0m"
 else
     echo "Valid Arguments are start or stop or restart ONLY"
-    echo -e "Ex: \n\t \e[31m bash $0 start  \e[0m"
+    echo -e "Ex: \n\t \e[33m bash $0 start  \e[0m"
 fi 
 
 echo -e "Script Completed"
