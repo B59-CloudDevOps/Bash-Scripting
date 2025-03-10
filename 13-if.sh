8dd343c0c9fe704c6f5236d599bb07b1e732499f
CodingManoj <CodingManoj@gmail.com>
2025-03-09 21:35:25 -0400

    Bash Scripting ~ AutoPush

--- 13-if.sh
+++ 13-if.sh
@@ -16,7 +16,7 @@ if [ "$ACTION" == "start" ]; then         # The below commands will only be exec
     echo "Starting the service"
 else
     echo "Valid Arguments are start"
-    echo -e "Ex: \n\t \e[33m bash $0 start \e[0m"
+    echo -e "Ex: \n\t \e[31m bash $0 start \e[0m"
 fi 
 
 echo -e "Script Completed"
